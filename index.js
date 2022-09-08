4c9fbc0fa5ce5839fd9a468fa7fbcd5491eebc67
Larry Weller <lweller86@gmail.com>
2022-09-08 18:30:29 -0400

    no where near done

--- index.js
+++ index.js
@@ -187,8 +187,35 @@ Use the game function below to do the following:
 RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
 */
 
+let computer = Math.random();
+
+  if(computer < 0.34) {
+    return = "rock";
+  }else if (computer >= 0.34 && <= 0.74) {
+    retunr = "paper";
+  }else if(computer >= 0.75 && <= 1.0) {
+    return = "scissors";
+  }
+
+
 function game(user, computer){
-  /*add your code here*/
+  if(user === computer){
+    return `it's a tie`;
+  }else if (user === 'rock' && computer === 'scissors') {
+    return = 'you win!';
+  }else if (user === 'rock' && computer === 'paper') {
+    return = 'you lose!';
+  }else if (user === 'paper' && computer === 'scissors') {
+    return = 'you lose!';
+  }else if (user === 'paper' && computer === 'rock') {
+    return = 'you win!';
+  }else if (user === 'scissors' && computer === 'rock') {
+    return = 'you lose!';
+  }else if (user === 'scissors' && computer === 'paper') {
+    return = 'you win!';
+  }else {
+    return = "try again"
+  }
 }
 
 
